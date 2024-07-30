c0090709a5240413a7fefa0be0286c28c1eb96f7
trindhol123-cell <gembrotkilagembrot@outlook.com>
2024-07-30 23:44:32 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '5443bf4c-a3dd-4cbb-8b3d-febc7a568364';
 
-const พร็อกซีไอพีs = ['192.9.177.204'];
+const พร็อกซีไอพีs = ['128.199.188.174'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
